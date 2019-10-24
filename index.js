3e026be83e8790b0eb24217d15d9854141eac63e
kbuchinskiy <kirillsmailbox@gmail.com>
2019-10-24 20:44:30 +0300

    vue client initiated

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express"
 import ParseService from "./parseService";
+import fs from "fs";
 
 const PORT = 7112;
 const URL = "https://www.parimatch.com/en/sport/futbol/anglija-premer-liga";
@@ -7,8 +8,13 @@ const URL = "https://www.parimatch.com/en/sport/futbol/anglija-premer-liga";
 const parseService = new ParseService(URL);
 
 const app = express();
+app.use('/static', express.static(__dirname + '/static'));
 
 app.get("/", (req, res) => {
+    fs.createReadStream(__dirname + "/client/dist/index.html").pipe(res);
+});
+
+app.get("/odds", (req, res) => {
     parseService.getData().then((data) => {
         res.send(data);
     });
