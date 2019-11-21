c6cc8f333ac5f2005553dc1285cb115a05af9b3b
kbuchinskiy <kirillsmailbox@gmail.com>
2019-11-22 00:29:34 +0200

    v-autoselect for team added

--- client/src/components/AddMatchweek.vue
+++ client/src/components/AddMatchweek.vue
@@ -6,12 +6,12 @@
           <v-text-field v-model="matchweekId" label="Matchweek" required></v-text-field>
         </v-col>
       </v-row>
-      <v-row v-for="(match, i) in 10" :key="i" justify="center">
-        <v-col cols="12" sm="2">
-          <v-text-field v-model="matches[i].teams[0]" label="Home" required></v-text-field>
+      <v-row  v-for="(match, i) in this.matches" :key="i" justify="center">
+        <v-col cols="12" sm="3">
+          <v-autocomplete :items="teams" label="Home"></v-autocomplete>
         </v-col>
-        <v-col cols="12" sm="2">
-          <v-text-field v-model="matches[i].teams[1]" label="Away" required></v-text-field>
+        <v-col cols="12" sm="3">
+          <v-autocomplete :items="teams" label="Away"></v-autocomplete>
         </v-col>
         <v-col cols="12" sm="1">
           <v-text-field v-model="matches[i].odds[0]" label="1" required></v-text-field>
@@ -24,8 +24,8 @@
         </v-col>
       </v-row>
       <v-row justify="center">
-        <v-col cols="12" sm="3">
-          <v-btn @click="submit">Create matchweek</v-btn>
+        <v-col cols="12" sm="2">
+          <v-btn @click="submit">Create</v-btn>
         </v-col>
       </v-row>
     </v-form>
@@ -35,11 +35,11 @@
 <script>
 import axios from 'axios';
 
-const GAMES_AMOUNT = 10;
 export default {
   data() {
     return {
       matchweekId: null,
+      teams: [],
       matches: [],
     };
   },
@@ -50,16 +50,23 @@ export default {
     },
   },
   created() {
-    axios.get('http://localhost:7113/matchweek/last').then((res) => {
-      this.matchweekId = res.data;
-    });
-
-    for (let i = 0; i < GAMES_AMOUNT; i++) {
+    for (let i = 0; i < 10; i++) {
       this.matches.push({
         teams: ['Chelsea', 'Arsenal'],
         odds: [1.33, 4.0, 3.7],
       });
     }
+
+    axios
+      .get('http://localhost:7113/matchweek/last')
+      .then((res) => {
+        this.matchweekId = res.data;
+      });
+    axios
+      .get('http://localhost:7113/teams')
+      .then((res) => {
+        this.teams = res.data;
+      });
   },
 };
 </script>
