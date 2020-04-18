9e5db7c9950bfc2372120c49330f8c022a8a50ed
Kirill <kirill@kirills-macbook.local>
2020-04-18 18:53:58 +0300

    vuex-persisted-state

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import createPersistedState from 'vuex-persistedstate';
 
 
 Vue.use(Vuex);
@@ -7,6 +8,9 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   strict: true,
+  plugins: [
+    createPersistedState(),
+  ],
   state: {
     token: null,
     user: null,
